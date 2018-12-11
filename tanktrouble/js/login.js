7331d75cca31a580f14e1dc7411bf9922dae6d78
Nasko-Kurtakov <nasko.kurtakov@gmail.com>
2018-12-11 02:25:51 +0200

    Demo setup with fb api

--- tanktrouble/js/login.js
+++ tanktrouble/js/login.js
@@ -1,17 +1,17 @@
 (function(){
-    login =  function(){
+    login = function(){
         FB.login(function (response) {
             if(response.status === "connected"){
                 FB.api(`/${response.authResponse.userID}`,{fields:"email,name"},function (userInfo) {
-
-                })
+                    console.log(userInfo);
+                    //log in at the server and save the responce to session storage or local storage
+                });
                 // document.location = "http://localhost:8080/PissTanks/tanktrouble/startGame.html"
             }
         },{scope: 'public_profile,email'})
     };
 
     document.getElementById("logInBtn").addEventListener("click",function (e) {
-        login()
+        login();
     });
 }());
-
