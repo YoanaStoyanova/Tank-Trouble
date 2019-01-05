52a5d02bc8f1af70ddd0af0766f3e281b5511cf3
YoanaStoyanova <yy.stoyanova@abv.bg>
2019-01-05 13:37:31 +0200

    Add https

--- Server/java-server/src/main/java/bg/tank/trouble/server/Main.java
+++ Server/java-server/src/main/java/bg/tank/trouble/server/Main.java
@@ -3,17 +3,22 @@ package bg.tank.trouble.server;
 import com.corundumstudio.socketio.Configuration;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
 
         Configuration httpConfig = new Configuration();
-        httpConfig.setHostname("localhost");
+        httpConfig.setHostname("0.0.0.0");
         httpConfig.setPort(8080);
 
         Configuration socketConfig = new Configuration();
-        socketConfig.setHostname("localhost");
+        socketConfig.setHostname("0.0.0.0");
         socketConfig.setPort(6969);
 
         /* set multithreading configuration */
