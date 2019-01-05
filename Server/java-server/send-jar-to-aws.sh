58cf3bb4856a94fde0627d381ba602c9dda0ab71
zshehov <z.shehov@gmail.com>
2019-01-05 18:03:21 +0200

    script 2000

--- Server/java-server/send-jar-to-aws.sh
+++ Server/java-server/send-jar-to-aws.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 scp -i "~/amazon/private-keys-m-390595c1-4ebc-4136-a807-4c280d450e5c-0.pem" ./target/TankTroubles-1.0-SNAPSHOT.jar ubuntu@54.154.212.251:/home/ubuntu/Tank-Trouble/Server/java-server/server.jar
 
+tar -czf static.tar.gz static/
 
+scp -i "~/amazon/private-keys-m-390595c1-4ebc-4136-a807-4c280d450e5c-0.pem" ./static.tar.gz ubuntu@54.154.212.251:/home/ubuntu/Tank-Trouble/Server/java-server/static.tgz
+
+rm static.tar.gz
